5253334408cd4279617e96a1e8b141f69dc815c9
isd-1 <1753741585@qq.com>
2025-08-05 21:17:53 +0800

    dirty fix for maraxsis 1.30.41 update
    Add k2so check.

--- compat/planets.lua
+++ compat/planets.lua
@@ -57,8 +57,12 @@ if mods["maraxsis"] then
         data.raw["assembling-machine"]["maraxsis-hydro-plant"].effect_receiver = { base_effect = { productivity = 0.25, quality = 2.5 }}
         data.raw.technology["aop-core-mining"].prerequisites = { "aop-electromechanics", "promethium-science-pack", "maraxsis-deepsea-research" }
         if settings.startup["aop-specialized-science"].value then
-            data.raw.technology["aop-specialized-science"].prerequisites = { "aop-armory", "aop-petrochemistry", "aop-hybridation", "maraxsis-deepsea-research" }
-        end 
+            if mods["Krastorio2-spaced-out"] then 
+                data.raw.technology["aop-specialized-science"].prerequisites = { "aop-armory", "aop-petrochemistry", "aop-hybridation", "maraxsis-research-vessel" }
+            else    
+                data.raw.technology["aop-specialized-science"].prerequisites = { "aop-armory", "aop-petrochemistry", "aop-hybridation", "maraxsis-deepsea-research" }
+            end 
+        end
     end
     data.raw.recipe["coal-synthesis"].category = "hydro-or-synthesis"
     add_crafting_categories("assembling-machine", "maraxsis-hydro-plant", {"hydro-or-synthesis", "hydraulics", "hydraulics-or-chemistry", "hydraulics-or-organic", "hydraulics-or-chemistry-or-cryogenics", "synthesis-or-chemistry"})
